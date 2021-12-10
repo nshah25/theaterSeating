77dbff8d23453f9003ad1cd6a2fe1554c742183e
nshah25 <68348722+nshah25@users.noreply.github.com>
2021-12-09 21:37:55 -0500

    Delete theaterSeatingInterface.java

--- theaterSeatingInterface.java
+++
@@ -1,7 +0,0 @@
-import java.io.IOException;
-
-public interface theaterSeatingInterface 
-{
-	String parseInputFile(String s) throws seatAllocationError;
-	String createFile() throws IOException;
-}
